d9bd826b10fcea8830a31a937092c98e224c54fe
Harishankar Giri <82793566+Hari-9249@users.noreply.github.com>
2021-12-15 21:08:36 +0530

    Add files via upload

---
+++ r.hari2.R
@@ -0,0 +1,35 @@
+height = 5
+width = 10
+area = height * width
+area
+ls()
+rm(height,width)
+ls()
+"this is basic of 'r'"
+this is basic of 'r'
+x = c(1,'hari',"hsg")
+x
+x[-3]
+x[2]
+x[-2]
+friends = c('muna','rakesh','kai','tara')
+friends[4]
+friends[-3]
+friends[-1:-3]
+a = 2
+a
+x = 'papu'
+x
+b = "'bbfbf''fhff''dg'"
+b
+a = 1L
+a
+class(a)
+s = seq(10)
+s
+x = 1+2i
+class(x)
+Re(x)
+Im(x)
+x = letters[1:3]
+x
